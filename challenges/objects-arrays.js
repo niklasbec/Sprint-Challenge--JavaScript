1fc2aad955fd94343fbf50e62438415c09e74e5b
Niklas Becker <niklasbeckerr@gmail.com>
2019-10-18 14:50:52 +0200

    x

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -88,7 +88,8 @@ const contactInfo = graduates.map((currentItem) => {
 });
 console.log(contactInfo);
 
-/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
+/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all.
+ This will be an array of objects. Log the result. */
 const unisWithUni = []
 graduates.findIndex((currentItem) => {
   if (currentItem.university.includes('Uni')) {
