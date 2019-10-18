99982662ac8f88099ad8f61a400f497b815a43f5
Niklas Becker <niklasbeckerr@gmail.com>
2019-10-18 09:54:12 +0200

    functions step 2 done

--- challenges/functions.js
+++ challenges/functions.js
@@ -16,6 +16,18 @@ const consume = function(a, b, cb) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = function (a, b) {
+  return a + b
+}
+
+const multiply = function (a, b) {
+  return a * b
+}
+
+
+const greeting = function (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
